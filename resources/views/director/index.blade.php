978e461ecd9e612251adc7ec0f79f3cd4db14ef5
Antonio Nogues Gómez <anoggom0910@g.educaand.es>
2026-05-07 11:37:50 +0200

    Implementar componente loader en lista de directores.

--- resources/views/director/index.blade.php
+++ resources/views/director/index.blade.php
@@ -7,19 +7,7 @@
                     class="relative overflow-x-auto shadow-md sm:rounded-lg border border-zinc-800">
 
                     {{-- Loader que se muestra cuando se haga la petición a la API --}}
-                    <div x-show="loading" class="p-10 text-center text-zinc-500 bg-zinc-900/40">
-                        <div class="flex flex-col items-center gap-2">
-                            <svg class="animate-spin h-5 w-5 text-indigo-400" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24">
-                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                    stroke-width="4"></circle>
-                                <path class="opacity-75" fill="currentColor"
-                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                                </path>
-                            </svg>
-                            <span>Cargando directores...</span>
-                        </div>
-                    </div>
+                    <x-loader text="Cargando directores..." />
 
                     <table x-show="!loading" class="w-full text-sm text-left text-zinc-400" x-cloak>
                         <thead class="text-xs uppercase bg-zinc-800/80 text-zinc-300">
