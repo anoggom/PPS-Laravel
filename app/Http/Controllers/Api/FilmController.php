760ae5d9fdf5c2847976d47e48f00899d0caebc3
Antonio Nogues Gómez <anoggom0910@g.educaand.es>
2026-05-12 18:50:19 +0200

    Actualizar controlador de API

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Film;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class FilmController extends Controller
 {
@@ -14,7 +15,7 @@ class FilmController extends Controller
     {
         // En la paginación esta el valor null, para que laravel se encargue automaticamente de asignar el número de pagina.
         $peliculas = Film::paginate(15, ['*'], 'page', null, count(Film::all()));
-        return response()->json($peliculas);
+        return response()->json($peliculas, 200);
     }
 
     /**
@@ -30,8 +31,12 @@ class FilmController extends Controller
      */
     public function show(int $id)
     {
-        $pelicula = Film::with('director')->findOrFail($id);
-        return response()->json($pelicula);
+        try {
+            $pelicula = Film::with('director')->findOrFail($id);
+            return response()->json($pelicula, 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json('Película no encontrada.', 404);
+        }
     }
 
     /**
